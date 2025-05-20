474aec91c6176344924236c66336ad09a5481e78
Asciugano <samuele.diaferio@fastwebnet.it>
2025-05-20 18:27:02 +0200

    aggiunte tutte le rotte per i messaggi

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -26,12 +27,41 @@ export const getMessages = async (req, res) => {
         { senderID: userToChatID, reciverID: myID },
       ],
     });
-        
-    res.status(200).json({messages})
+
+    res.status(200).json({ messages });
   } catch (error) {
     console.error("error in getMessages", error.message);
     res.status(500).json({ message: "internal server error" });
   }
 };
 
-export const sendMessage = async (req, res) => {};
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: reciverID } = req.params;
+    const senderID = req.user._id;
+
+    let imgUrl;
+    if (img) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imgUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = Message({
+      senderID: senderID,
+      reciverID: reciverID,
+      text: text,
+      image: imgUrl,
+    });
+
+    await newMessage.save();
+
+    // todo
+    // realtime functionality
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("error in sendMessage", error.message);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
