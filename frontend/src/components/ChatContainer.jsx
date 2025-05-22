4cc6f7d09d3a3497e1459233920bde02abf98789
Asciugano <samuele.diaferio@fastwebnet.it>
2025-05-22 18:54:07 +0200

    create la home page (non funzionante(colpa della sidebar))

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ChatContainer = () => {};
+
+export default ChatContainer;
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { axiosInstace } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstace.get("/messages/users");
+            console.log('res: ',res);
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+
+  getMessages: async (userID) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = axiosInstace.get(`messages/${userID}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  setSelectedUser: (user) => set({ selectedUser: user }),
+}));
